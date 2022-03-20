261c7827ff4fe88304cba7e5caf554f230fc4add
Diego Miguel <dmlls@diegomiguel.me>
2022-03-20 12:08:30 +0100

    Import casing functions explicitly

--- casefy/__init__.py
+++ casefy/__init__.py
@@ -1,1 +1,18 @@
-from .casefy import *
+from .casefy import (
+    camelcase,
+    pascalcase,
+    snakecase,
+    constcase,
+    kebabcase,
+    upperkebabcase,
+    separatorcase,
+    sentencecase,
+    titlecase,
+    alphanumcase,
+    lowercase,
+    uppercase,
+    capitalcase
+)
+
+# Don't expose the submodule itself
+del globals()["casefy"]
